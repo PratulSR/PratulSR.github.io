7f75c326b54c5bda45db3f663cb524c5abe36603
Pratul Singh Raghava <pratulsr26@gmail.com>
2023-05-19 16:27:46 +1000

    New website

--- js/main.js
+++ js/main.js
@@ -54,5 +54,15 @@
         }
     });
 
+
+    // Payment methods show hide
+    $('.checkout .payment-method .custom-control-input').change(function () {
+        if ($(this).prop('checked')) {
+            var checkbox_id = $(this).attr('id');
+            $('.checkout .payment-method .payment-content').slideUp();
+            $('#' + checkbox_id + '-show').slideDown();
+        }
+    });
+    
 })(jQuery);
 
