82ec76acce1eeca7f3be704ba00b00c9ef71dcb1
Pratul Singh Raghava <pratulsr26@gmail.com>
2023-11-20 22:21:42 +1100

    Revert "Gantt website uploaded"
    This reverts commit 53ebdc2f3da1f20e67c1bf143302b209de20c8e2.

--- Java/src/main/java/com/elec5619/gantt/tables/ganttChart/GanttChartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.elec5619.gantt.tables.ganttChart;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface GanttChartRepository extends CrudRepository<GanttChart, Integer> {
-}
--- Java/src/main/java/com/elec5619/gantt/tables/task/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.elec5619.gantt.tables.task;
-
-import com.elec5619.gantt.tables.task.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository<Task, Integer> {
-}
